d705cfde78eeeb004bcaf881517a0dfa2e0f11f6
Silvio Paganini <>
2021-06-30 14:05:20 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   gof-gpu v2.0.3
+ *   gof-gpu v2.0.4
  *   https://github.com/silviopaganini/game-of-life-gpu
  *
  *   Copyright (c) Silvio Paganini (https://github.com/silviopaganini) and project contributors.
